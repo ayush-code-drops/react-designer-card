e4ca34db5145fbe94b613977e948b3ff91d9b606
Ayush Agrawal <81424725+ayush-code-drops@users.noreply.github.com>
2022-01-07 14:49:47 +0530

    static card assignment added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Card } from "./components/Card";
+import { StaticCard } from "./components/StaticCard";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <h4>Designer Card</h4>
+
       <Card />
+
+      <hr />
+      <h4>Static Card</h4>
+      <StaticCard />
     </div>
   );
 }
---
+++ src/components/StaticCard.jsx
@@ -0,0 +1,20 @@
+export function StaticCard() {
+  return (
+    <div>
+      <h2>Mobile Operating System</h2>
+      <ul>
+        <li>Android</li>
+        <li>Ios</li>
+        <li>Windows</li>
+        <li>Linux</li>
+      </ul>
+      <h2>Mobile Manufacturers</h2>
+      <ul>
+        <li>Samsung</li>
+        <li>Nokia</li>
+        <li>Apple</li>
+        <li>Vivo</li>
+      </ul>
+    </div>
+  );
+}
